948048669d8e84ea59aaf2f6d5e98a6fbef63b22
Sarah Abouali <sarahabouali7@gmail.com>
2026-05-21 14:52:35 +0100

    Delete main.c.py

--- main.c.py
+++
@@ -1,9 +0,0 @@
-# Program to check if a word is a palindrome
-
-word = input("Enter a word: ")
-word = word.lower()
-
-if word == word[::-1]:
-    print(f'"{word}" is a palindrome.')
-else:
-    print(f'"{word}" is not a palindrome.')
